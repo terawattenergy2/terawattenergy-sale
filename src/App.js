b90693318eac631e1ddc97685af51dd9aa009235
TitawanUdh <titawan.udh@gmail.com>
2026-08-21 17:29:10 +0700

    update5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
   const [mode, setMode] = useState("wizard");
   const [theme, setTheme] = useState("light");
   const SHEET_URL =
-    "https://script.google.com/macros/s/AKfycbwjlVYgeItIfnX2DtPusdvBhnldd0O8-zCNQkF1ywy5fC0GcuT8blZmnI5aYbXh9jkb-Q/exec";
+    "https://script.google.com/macros/s/AKfycbzzhFSGXDDT10pd20X0K64YPOx7-eu9p-A6vnzItZ_jW1I4foCsVQ2UJ-qrQcBBaskQhw/exec";
+    // https://script.google.com/macros/s/AKfycbzzhFSGXDDT10pd20X0K64YPOx7-eu9p-A6vnzItZ_jW1I4foCsVQ2UJ-qrQcBBaskQhw/exec
 
   return (
     <div className={`main-page ${theme === "dark" ? "dark" : ""}`}>
